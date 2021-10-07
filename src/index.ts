d9bd23a45d71825a3c0b8454fcf4d0a64fca2ce9
matias9000 <92063954+matias9000@users.noreply.github.com>
2021-10-06 21:30:44 -0300

    2 logs mas

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ console.log("que capo, ya estoy usando github y voy a hacer mi primer commit");
 console.log(10 + 25);
 console.log("otro comentario");
 console.log(200 + 500 - (250 * 5) / 2);
+console.log("test2")
