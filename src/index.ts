fa5340c1d3be9898ca97bdc950a7c8be29207b60
matias9000 <92063954+matias9000@users.noreply.github.com>
2021-10-06 21:28:06 -0300

    calculos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 console.log("test");
 console.log("que capo, ya estoy usando github y voy a hacer mi primer commit");
 //comentario agregado
+console.log(10 + 25);
+console.log("otro comentario");
+console.log(200 + 500 - (250 * 5) / 2);
